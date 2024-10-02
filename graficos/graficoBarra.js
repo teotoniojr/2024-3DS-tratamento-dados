11452e882ace802dae0df82193d45b0fafc24823
Teotonio José de Souza Junior <102707827+teotoniojr@users.noreply.github.com>
2024-10-02 10:19:06 -0300

    Add files via upload

--- graficos/graficoBarra.js
+++ graficos/graficoBarra.js
@@ -1,10 +1,24 @@
 
 async function criarGraficoBarra(){
-    const url = 'https://raw.githubusercontent.com/teotoniojr/2024-3DS-API-ex/refs/heads/main/jogos_mais_vendidos_gemini.json'
+    const url = 'https://raw.githubusercontent.com/teotoniojr/2024-3DS-API-ex/refs/heads/main/jogo_vendas_gemini.json'
     const res = await fetch(url)
     const dados = await res.json()
+    const jogos = Object.keys(dados)
+    const vendas = Object.values(dados)
 
-    console.log(dados)
+    const data = [
+        {
+            x: jogos,
+            y: vendas,
+            type: 'bar'
+        }
+    ]
+    
+    const grafico = document.createElement('div')
+    grafico.className = 'grafico'
+    document.getElementById('caixa-grafico').appendChild(grafico)
+    Plotly.newPlot(grafico, data)
+
 }
 
 criarGraficoBarra()
